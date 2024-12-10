8861ae080472d82ac752923cd5ec341a151222d2
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2024-12-10 14:53:10 +0500

    add index file.

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+    console.log(req)
+
+    res.send('Welcome to the first api')
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running on port" + PORT)
+})
